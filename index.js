4d1a4a5d37913aba14bcb8505512b06cf224052e
oseji <82390694+oseji@users.noreply.github.com>
2023-01-20 16:55:04 +0100

    added theme 1 and 3

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const multiply = document.getElementById("multiply");
 const decimalPoint = document.getElementById("decimal-point");
 
 const reset = document.querySelector(".reset");
+const deleteBtn = document.querySelector(".del-button");
 const equals = document.querySelector(".equals");
 
 //slider components
@@ -26,6 +27,29 @@ const themeSection = document.querySelector(".theme-section");
 const appName = document.querySelector(".app-name");
 
 //slider code
+//theme 1
+themeOne.addEventListener("click", function () {
+  body.style.backgroundColor = "rgb(51 65 85 )";
+  themeSection.style.color = "white";
+  themeSlider.style.backgroundColor = "rgb(30 41 59)";
+  sliderDot.style.backgroundColor = "rgb(220 38 38)";
+  appName.style.color = "white";
+  outputDisplay.style.backgroundColor = "rgb(15 23 42)";
+  outputDisplay.style.color = "white";
+  btnSection.style.backgroundColor = "rgb(30 41 59 )";
+  btnNumber.forEach(function (i) {
+    i.style.backgroundColor = "rgb(203 213 225)";
+    i.style.color = "rgb(51 65 85 )";
+  });
+  otherSpecialButtons.forEach(function (i) {
+    i.style.backgroundColor = "#E3E3E3";
+    i.style.color = "#3B3B3B";
+  });
+  reset.style.backgroundColor = "rgb(7 89 133)";
+  deleteBtn.style.backgroundColor = "rgb(7 89 133)";
+  equals.style.backgroundColor = "rgb(220 38 38)";
+});
+
 //theme 2
 themeTwo.addEventListener("click", function () {
   body.style.backgroundColor = "#E3E3E3";
@@ -38,11 +62,37 @@ themeTwo.addEventListener("click", function () {
   btnSection.style.backgroundColor = "#D0CFCF";
   btnNumber.forEach(function (i) {
     i.style.backgroundColor = "#E3E3E3";
+    i.style.color = "#3B3B3B";
   });
   otherSpecialButtons.forEach(function (i) {
     i.style.backgroundColor = "#E3E3E3";
+    i.style.color = "#3B3B3B";
   });
   reset.style.backgroundColor = "#45ADB0";
+  deleteBtn.style.backgroundColor = "#45ADB0";
+  equals.style.backgroundColor = "#E9A95F";
+});
+
+//theme 3
+themeThree.addEventListener("click", function () {
+  body.style.backgroundColor = "#260124";
+  themeSection.style.color = "#F2FE3F";
+  themeSlider.style.backgroundColor = "#511480";
+  sliderDot.style.backgroundColor = "#21CEE9";
+  appName.style.color = "#F2FE3F";
+  outputDisplay.style.backgroundColor = "#511480";
+  outputDisplay.style.color = "#F2FE3F";
+  btnSection.style.backgroundColor = "#511480";
+  btnNumber.forEach(function (i) {
+    i.style.backgroundColor = "#5E005B";
+    i.style.color = "#F2FE3F";
+  });
+  otherSpecialButtons.forEach(function (i) {
+    i.style.backgroundColor = "#5E005B";
+    i.style.color = "#F2FE3F";
+  });
+  reset.style.backgroundColor = "#9F009A";
+  deleteBtn.style.backgroundColor = "#9F009A";
   equals.style.backgroundColor = "#E9A95F";
 });
 
